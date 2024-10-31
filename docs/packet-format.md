c17904a9e61bb168a98e58e7a7fd3a64add259da
Jasper <japsu.honkasalo@gmail.com>
2024-10-31 12:08:15 +0200

    Change packet format, implement packet class

--- docs/packet-format.md
+++ docs/packet-format.md
@@ -7,9 +7,8 @@ A custom binary format is used to send data between the client and the game serv
 
 The packet structure is as follows:
 
-|        Bits |  Field  | Description                        |
-|------------:|:-------:|:-----------------------------------|
-|     0-7 (8) | version | The version of the packet format.  |
-|    8-15 (8) |  type   | The type of the packet.            |
-|  16-31 (16) | length  | The length of the payload.         |
-|       32... | payload | The data payload of the packet.    |
+|     Bits |  Field  | Description                        |
+|---------:|:-------:|:-----------------------------------|
+|  0-7 (8) | version | The version of the packet format.  |
+| 8-15 (8) |  type   | The type of the packet.            |
+|    16... | payload | The data payload of the packet.    |
---
+++ src/Server/Packet.cs
@@ -0,0 +1,59 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+
+namespace Server;
+
+internal readonly struct Packet // Could also be a pooled class
+{
+    private const byte FORMAT_VERSION = 1;
+    private const int HEADER_LENGTH = 4;
+    
+    public readonly Guid PlayerSessionId;
+    public readonly byte Version;
+    public readonly byte Type;
+    public readonly ArraySegment<byte> Data;
+    
+    
+    private Packet(Guid playerSessionId, byte version, byte type, ArraySegment<byte> data)
+    {
+        PlayerSessionId = playerSessionId;
+        Version = version;
+        Type = type;
+        Data = data;
+    }
+    
+    
+    public static bool TryCreate(Guid playerSessionId, byte[] buffer, int offset, int size, out Packet packet)
+    {
+        Debug.Assert(size >= HEADER_LENGTH, "Received buffer is too short");
+        
+        byte version = buffer[offset];
+        if (version != FORMAT_VERSION)
+        {
+            packet = default;
+            return false;
+        }
+        
+        byte type = buffer[offset + 1];
+        
+        ArraySegment<byte> data;
+        int payloadSize = size - HEADER_LENGTH;
+        if (payloadSize > 0)
+        {
+            byte[] dataBuffer = ArrayPool<byte>.Shared.Rent(payloadSize);
+
+            int payloadStart = offset + HEADER_LENGTH;
+            Buffer.BlockCopy(buffer, payloadStart, dataBuffer, 0, payloadSize);
+        
+            data = new ArraySegment<byte>(dataBuffer, 0, payloadSize);
+        }
+        else
+        {
+            data = [];
+        }
+        
+        packet = new Packet(playerSessionId, version, type, data);
+        
+        return true;
+    }
+}
