0ea597e3c3225e5d7ec24f69472f05513a4a2eb5
Jasper <japsu.honkasalo@gmail.com>
2024-11-04 10:34:02 +0200

    Implement NetMessage and MessageManager to assign IDs for registered messages

---
+++ src/Shared/Networking/Messages/MessageManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+
+namespace Shared.Networking.Messages;
+
+internal static class MessageManager
+{
+    /// <summary>
+    /// Uses reflection to find all NetMessage types, and registers them.
+    /// </summary>
+    public static void RegisterAllMessages()
+    {
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        
+        // Sort the assemblies by name to ensure consistent message IDs.
+        Array.Sort(assemblies, (a, b) => string.Compare(a.FullName, b.FullName, StringComparison.Ordinal));
+        
+        foreach (Assembly assembly in assemblies)
+        {
+            Type[] types = assembly.GetTypes();
+            
+            // Sort the types by name to ensure consistent message IDs.
+            Array.Sort(types, (a, b) => string.Compare(a.FullName, b.FullName, StringComparison.Ordinal));
+            
+            foreach (Type type in types)
+            {
+                if (type.IsSubclassOf(typeof(NetMessage)) && !type.IsAbstract)
+                    NetMessages.Register(type);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Provides methods for retrieving message IDs for message types.
+    /// </summary>
+    internal static class NetMessages
+    {
+        private static byte nextId;
+        private static readonly Dictionary<Type, byte> MessageIds = new();
+        private static readonly Dictionary<byte, Func<NetMessage>> MessageCreators = new();
+
+        public static byte GetId<T>() => GetId(typeof(T));
+
+
+        public static byte GetId(Type type)
+        {
+            if (MessageIds.TryGetValue(type, out byte id))
+                return id;
+            
+            throw new InvalidOperationException($"No message ID found for type {type}.");
+        }
+
+
+        public static NetMessage CreateInstance(byte id)
+        {
+            if (MessageCreators.TryGetValue(id, out Func<NetMessage>? creator))
+                return creator();
+
+            throw new InvalidOperationException($"No message type registered for ID {id}, cannot create instance.");
+        }
+        
+        
+        internal static void Register(Type type)
+        {
+            if (MessageIds.TryGetValue(type, out byte id))
+                return;
+
+            if (nextId == byte.MaxValue)
+                throw new InvalidOperationException("Ran out of message IDs. Cannot register more message types.");
+
+            id = nextId++;
+            MessageIds[type] = id;
+
+            MessageCreators[id] = () => Activator.CreateInstance(type) as NetMessage ?? throw new InvalidOperationException("Failed to create message instance.");
+        }
+    }
+}
---
+++ src/Shared/Networking/Messages/NetMessage.cs
@@ -0,0 +1,46 @@
+﻿using NetStack.Serialization;
+
+namespace Shared.Networking.Messages;
+
+/// <summary>
+/// Represents a message (packet) that can be sent over the network.<br/>
+/// 
+/// <remarks>
+/// Implementations must be thread safe.<br/>
+/// You should not cache instances of this class, as it is part of a pool and will be reused.<br/>
+/// Any constructors will be skipped when deserializing, so don't rely on them being called.
+/// </remarks>
+/// </summary>
+public abstract class NetMessage
+{
+    public abstract byte Id { get; }
+
+
+    protected NetMessage()
+    {
+        
+    }
+    
+    
+    public void Serialize(BitBuffer buffer)
+    {
+        buffer.AddByte(Id);
+        SerializeInternal(buffer);
+    }
+    
+    
+    public bool Deserialize(BitBuffer buffer)
+    {
+        return DeserializeInternal(buffer);
+    }
+
+
+    protected abstract void SerializeInternal(BitBuffer buffer);
+    protected abstract bool DeserializeInternal(BitBuffer buffer);
+
+
+    public override string ToString()
+    {
+        return $"{GetType().Name} (ID: {Id})";
+    }
+}
