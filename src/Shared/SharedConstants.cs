87f46113879f500fd0f9dc13dda89b2e6f4ee7e0
Jasper <japsu.honkasalo@gmail.com>
2024-11-11 10:26:12 +0200

    Change default port to 11221

--- src/Shared/SharedConstants.cs
+++ src/Shared/SharedConstants.cs
@@ -3,7 +3,7 @@
 public static class SharedConstants
 {
     public const ushort GAME_VERSION = 1;
-    public const int SERVER_PORT = 1221;
+    public const int SERVER_PORT = 11221;
     public const int MAX_PACKET_SIZE_BYTES = 2048;
     public const string DEVELOPMENT_AUTH_PASSWORD = "developer";
 }
