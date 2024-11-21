57a52f3426389a8fadae7a9f366a3618972d980f
Jasper <japsu.honkasalo@gmail.com>
2024-11-21 12:57:10 +0200

    Fix ScaleNet game version datatype to uint

--- src/ScaleNet.Common/SharedConstants.cs
+++ src/ScaleNet.Common/SharedConstants.cs
@@ -3,7 +3,7 @@
     public static class SharedConstants
     {
         // Game
-        public const ushort GAME_VERSION = 3;
+        public const uint GAME_VERSION = 3;
     
         // Networking
         public const int MAX_PACKET_SIZE_BYTES = 2048;
