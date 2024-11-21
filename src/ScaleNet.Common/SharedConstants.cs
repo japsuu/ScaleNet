5c671782e41d95f0bab3f203f8da81b308edbc71
Jasper <japsu.honkasalo@gmail.com>
2024-11-21 17:04:36 +0200

    Remove auth stuff from ScaleNet shared constants

--- src/ScaleNet.Common/SharedConstants.cs
+++ src/ScaleNet.Common/SharedConstants.cs
@@ -4,11 +4,5 @@
     {
         // Networking
         public const int MAX_PACKET_SIZE_BYTES = 2048;
-    
-        // Account registration
-        public const int MIN_USERNAME_LENGTH = 3;
-        public const int MAX_USERNAME_LENGTH = 16;
-        public const int MIN_PASSWORD_LENGTH = 6;
-        public const int MAX_PASSWORD_LENGTH = 32;
     }
 }
