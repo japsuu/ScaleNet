03a0faccf884abb5e518e9e14453c6cebddf28f0
Jasper <japsu.honkasalo@gmail.com>
2024-12-19 10:54:56 +0200

    Add Bayou WS lib as client base

---
+++ src/ScaleNet.Client/LowLevel/Transport/WebSocket/WebSocketClientTransport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ScaleNet.Common;
+
+namespace ScaleNet.Client.LowLevel.Transport.WebSocket
+{
+    public class WebSocketClientTransport : IClientTransport
+    {
+        public string Address { get; set; }
+        public int Port { get; set; }
+        
+        public event Action<ConnectionStateArgs>? ConnectionStateChanged;
+        public event Action<DeserializedNetMessage>? MessageReceived;
+        
+        
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        public void ConnectClient()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void ReconnectClient()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void DisconnectClient()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void SendAsync<T>(T message) where T : INetMessage
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
